27a0876e6c55ceda995cbcea34343ba7f6b78244
milovansorich <116596359+milovansorich@users.noreply.github.com>
2022-10-27 10:16:28 -0300

    Create botilleria.py

---
+++ botilleria.py
@@ -0,0 +1,18 @@
+class products
+{
+  bar_code= #codigo de barras
+  price= #precio del producto
+  expiration_date= #fecha de vencimiento
+  brand= #marca
+  amount= #cantidad de productos
+  product_name= #nombre del producto
+  
+  ##fin seccion atributos##
+  
+  ##metodos##
+  ventas
+  
+  
+
+class customers 
+  
