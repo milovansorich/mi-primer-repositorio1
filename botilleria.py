3196c7d3152c3b39c781acde4e44ee07d7fcbe9b
milovansorich <116596359+milovansorich@users.noreply.github.com>
2022-10-31 15:03:58 -0300

    Pseudocodigo Botilleria

--- botilleria.py
+++ botilleria.py
@@ -6,34 +6,31 @@ class product
   brand= #marca
   price= #precio del producto
   
-  ##fin seccion atributos##
+    ##fin seccion atributos##
   
-  ##metodos##
+    ##metodos##
   get_code()
   get_bar_code()
   get_name()
   get_brand()
   get_price()
   
-  set_code(code)
-  set_bar_code(bar_code)
-  set_name(name)
-  set_brand(brand)
-  set_price(price)
+    ##Fin seccion Metodos##
 }  
   
 class stock
 {
   product= #clase producto
   quantity= #cantidad de este producto
-    
-  ##metodos##
+  
+   ##fin seccion atributos##
+   
+   ##metodos##
 
   get_product()
   get_quantity()
-  
-  set_product(product)
-  set_quantity(quantity)
+    
+    ##Fin seccion Metodos##
 }
 
 class supplier
@@ -42,15 +39,15 @@ class supplier
   name= #nombre del proveedor
   email= #correo electronico del proveedor
   
-  ##metodos##
+    ##fin seccion atributos##
+    
+    ##metodos##
   
   get_code()
   get_name()
   get_email()
   
-  set_code(code)
-  set_name(name)
-  set_email(email)  
+    ##Fin seccion Metodos##
 }
  
 class supplying
@@ -61,8 +58,9 @@ class supplying
   arrives_at= #fecha de llegada
   arrived= #indica si llego o no
   
-  
-  ##metodos##
+    ##fin seccion atributos##
+   
+    ##metodos##
   
   get_supplier()
   get_product()
@@ -70,11 +68,7 @@ class supplying
   get_arrives_at()
   get_arrived()
   
-  set_supplier(supplier)
-  set_product(product)
-  set_quantity(quantity)
-  set_arrives_at(arrives_at)
-  set_arrived(arrived) 
+    ##Fin seccion Metodos##
 }
 
 class customer
@@ -83,50 +77,15 @@ class customer
   first_name= #nombre del cliente
   last_name= #apellido paterno  del cliente
   email= #correo electronico del cliente
-   
-  ##metodos##
+    
+    ##fin seccion atributos##
+    
+    ##metodos##
   
   get_code()
   get_first_name()
   get_last_name()
   get_email()
   
-  set_code(code)
-  set_first_name(first_name)
-  set_last_name(last_name)
-  set_email(email) 
-}
-
-class order
-{
-  code= #codigo de la orden
-  ordered_at= #fecha de la orden
-  total= #total a pagar de la orden
-  
-   ##metodos##
-  
-  get_code()
-  get_ordered_at()
-  get_total()
-   
-  set_code(code)
-  set_ordered_at(ordered_at)
-  set_total(total)
-}
-
-class product_order
-{
-  order= #clase orden
-  product= #clase producto
-  quantity= #cantidad de productos pedidos
-  
-   ##metodos##
-  
-  get_order()
-  get_product()
-  get_quantity()
-  
-  set_order(order)
-  set_product(product)
-  set_quantity(quantity) 
+    ##Fin seccion Metodos##
 }
